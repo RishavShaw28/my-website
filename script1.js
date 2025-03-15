457bc9ba31b024aed61a8515d7158b66ff106b18
RishavShaw28 <rishavshaw143@gmail.com>
2025-03-15 16:40:19 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 function checkName() {
     var name = document.getElementById("nameInput").value.trim().toLowerCase();
     if (name === "rishav" || name === "prachi") { 
-        window.location.href = "page2.html"; 
+        window.location.href = "index.html"; 
     } else {
         document.getElementById("message").innerText = "This website is not for you.";
     }
