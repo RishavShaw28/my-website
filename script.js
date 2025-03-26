d3bce8d2dfc2a6611ecbc208b9d5b1160f19529b
RishavShaw28 <rishavshaw143@gmail.com>
2025-03-26 21:21:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function moveRandomE1(elm) {
-    elm.style.position ="absolute";
-    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * (window.innerHeight - elm.clientHeight)) + "px";
+    elm.style.left = Math.floor(Math.random() * (window.innerWidth - elm.clientWidth)) + "px";
 }
 
 const moveRandom = document.querySelector("#move-random");
 
 moveRandom.addEventListener("mouseenter", function(e) {
+    moveRandomE1(e.target);
+});
+
+
+moveRandom.addEventListener("touchstart", function(e) {
     moveRandomE1(e.target);
 });
